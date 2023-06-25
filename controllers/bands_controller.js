61e8ad5f76bb7e3228e9241915882367d63350e8
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-06-25 13:55:12 -0700

    Added querey to filter by band name

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,11 +2,23 @@
 const bands = require('express').Router();
 const db = require('../models');
 const { Band } = db;
+const { Op } = require('sequelize');
 
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll();
+        const searchTerm = req.query.name ? req.query.name : '';
+        const foundBands = await Band.findAll({
+            order: [
+                ['available_start_time', 'ASC'],
+                ['name', 'ASC']
+            ],
+            where: {
+                name: {
+                    [Op.iLike]: `%${searchTerm}%`
+                }
+            }
+        });
         res.status(200).json(foundBands);
     } catch (error) {
         res.status(500).json(error)
