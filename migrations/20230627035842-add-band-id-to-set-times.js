a26ef59c1f9f012e3459d427687290f2ac42f452
ramos-andres <102202582+ramos-andres@users.noreply.github.com>
2023-06-26 21:28:47 -0700

    Defined associations between models

---
+++ migrations/20230627035842-add-band-id-to-set-times.js
@@ -0,0 +1,15 @@
+'use strict'; 
+const { DataTypes } = require('sequelize');
+
+/** @type {import('sequelize-cli').Migration} */ 
+module.exports = { 
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('set_times', 'band_id', { 
+      type: DataTypes.INTEGER 
+    }); 
+  }, 
+  
+  async down (queryInterface, Sequelize) { 
+    await queryInterface.removeColumn('set_times', 'band_id'); 
+  } 
+};
--- models/event.js
+++ models/event.js
@@ -9,8 +9,13 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Stage, StageEvent }) {
       // define association here
+      Event.belongsToMany(Stage, {
+        foreignKey: 'event_id',
+        as: 'stages',
+        through: StageEvent,
+      })
     }
   }
   Event.init({
